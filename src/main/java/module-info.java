1a84e08b69a53e66e5b395c3c896f36bc7a6a4b8
Derek <DerekRoseRod@gmail.com>
2024-09-25 23:51:09 -0500

    Creacion del proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module co.edu.uniquindio.seguimiento2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens co.edu.uniquindio.seguimiento2 to javafx.fxml;
+    exports co.edu.uniquindio.seguimiento2;
+}
