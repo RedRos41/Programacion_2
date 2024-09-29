b51218c12c8b3a4ebc888f11b9f164178766e7d9
brahian stiven toro <torobrahian13@gmail.com>
2024-09-29 01:13:26 -0500

    ( IGNORAR EL MAL USO DEL VOCABULARIO EN LAS VALIDACIONES ) en ContactoPrincipal; implementacion de los metodos para obterner,editar,eliminar,listar y buscar los contactos

--- src/main/java/co/edu/uniquindio/seguimiento2/modelos/ContactoPrincipal.java
+++ src/main/java/co/edu/uniquindio/seguimiento2/modelos/ContactoPrincipal.java
@@ -16,15 +16,15 @@ public class ContactoPrincipal {
 
     public void agregarContacto(String nombre, String apellido, String telefono, LocalDate cumpleaños, String correoElectronico, String fotoUrl) throws Exception {
         if (nombre.isEmpty() || apellido.isEmpty() || telefono.isEmpty() || correoElectronico.isEmpty()) {
-            throw new Exception("estos campos deben ser ingresados.");
+            throw new Exception("deje la pereza y copie todo o si no pailas, no le creo ningun contacto");
         }
 
         if (!Pattern.matches("\\d{10}", telefono)) {
-            throw new Exception("El número de teléfono debe tener 10 dígitos numéricos");
+            throw new Exception("el numero no es de noruega o de republica checa, es de 10 numeritos");
         }
 
         if (!Pattern.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", correoElectronico)) {
-            throw new Exception("El formato del correo electrónico es inválido");
+            throw new Exception("Ese correo esta como rarito jmm");
         }
 
         Contacto contacto = Contacto.builder()
@@ -39,4 +39,84 @@ public class ContactoPrincipal {
 
         contactos.add(contacto);
     }
+
+    public int obtenerContacto(String id){
+
+        for (int i = 0; i < contactos.size(); i++) {
+            if( contactos.get(i).getId().equals(id) ){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void eliminarContacto (String id)throws Exception{
+        int posContacto = obtenerContacto(id);
+
+        if(posContacto == -1){
+            throw new Exception("que va a borrar si eso no existe, brut@");
+        }
+
+        contactos.remove(contactos.get(posContacto));
+
+    }
+
+    public void editarContacto (String id, String nombre, String apellido, String telefono, LocalDate cumpleaños, String correoElectronico, String fotoUrl)throws Exception{
+        
+        int posContacto = obtenerContacto(id);
+
+        if(posContacto == -1){
+            throw new Exception("oe tont@, no existe un contacto con esa ID, busque bien ");
+        }
+
+        if (nombre.isEmpty() || apellido.isEmpty() || telefono.isEmpty() || correoElectronico.isEmpty()) {
+            throw new Exception("deje la pereza y copie todo o si no pailas, no le edito ningun contacto");
+        }
+
+        if (!Pattern.matches("\\d{10}", telefono)) {
+            throw new Exception("el numero no es de noruega o de republica checa, es de 10 numeritos");
+        }
+
+        if (!Pattern.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", correoElectronico)) {
+            throw new Exception("Ese correo esta como rarito jmm");
+        }
+
+        Contacto contactoModificado = contactos.get(posContacto);
+        contactoModificado.setNombre(nombre);
+        contactoModificado.setApellido(apellido);
+        contactoModificado.setTelefono(telefono);
+        contactoModificado.setCumpleaños(cumpleaños);
+        contactoModificado.setCorreoElectronico(correoElectronico);
+        contactoModificado.setFotoUrl(fotoUrl);
+
+        contactos.set(posContacto, contactoModificado);
+
+    }
+
+    public List<Contacto> listarContactos(){
+        return contactos;
+    }
+
+    public List<Contacto> buscarContactosNombre(String nombre){
+        List<Contacto> contactosEncontrados = new ArrayList<>();
+
+        for(Contacto c : contactos){
+
+            if(c.getNombre().equals(nombre));
+            contactosEncontrados.add(c);
+        }
+        return contactosEncontrados;
+    }
+
+    public List<Contacto> buscarContactosTelefono(String telefono){
+        List<Contacto> contactosEncontrados = new ArrayList<>();
+
+        for(Contacto c : contactos){
+
+            if(c.getTelefono().equals(telefono));
+            contactosEncontrados.add(c);
+        }
+        return contactosEncontrados;
+    }
+
 }
