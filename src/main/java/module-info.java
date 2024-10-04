17f0f836e190d111ee78efc65929d0668b70a2be
brahian stiven toro <torobrahian13@gmail.com>
2024-10-03 21:03:39 -0500

    Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module co.edu.uniquindio.clinica {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens co.edu.uniquindio.clinica to javafx.fxml;
+    exports co.edu.uniquindio.clinica;
+}
